23aa532d80f90db1948b6af4558b3db2e73137c7
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-01-15 21:43:30 +0530

    Add files via upload

---
+++ 04. Array/insertOperation.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void display(int *a,int s){
+    for(int i=0;i<s;i++){
+        cout<<a[i]<<" ";
+    }
+
+    cout<<endl<<endl;
+}
+int main(){
+    int size =6;
+    int arr[size]={4,52,6,2,4};
+
+    display(arr,size);
+     // insert(index,data)
+     int index=3; // index where we have to insert the data
+     int data=45;  // data which we have to insert
+
+     for (int i = size; i > index; i--)
+     {
+
+        cout<<i<<endl;
+        arr[i]=arr[i-1];
+     }
+        arr[index]=data;
+     
+
+    display(arr,size);
+     
+     
+
+
+return 0;
+}
