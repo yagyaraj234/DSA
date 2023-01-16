4fd9459e0ba7918287ab7e32a547269689d89522
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-01-16 22:37:39 +0530

    Add files via upload

---
+++ linearSearch.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void display(int *a,int s){
+    for(int i=0;i<s;i++){
+        cout<<a[i]<<" ";
+    }
+
+    cout<<endl<<endl;
+}
+void input(int *a,int size){
+
+    for (int i = 0; i < size; i++)
+    {
+        cin>>a[i];
+    }
+    
+}
+
+int linearSearch(int *a,int s,int value){
+    for (int i = 0; i < s; i++)
+    {
+        if(a[i]==value)
+          return i;
+        
+        
+    }
+    
+}
+
+int main(){
+
+    int value=5;
+    int s;
+    cout<<"Enter the size of an array : ";
+    cin>>s;
+    int arr[s];
+    cout<<"Input the array"<<endl;
+    input(arr,s);
+  cout<<  linearSearch(arr,s,value);
+
+
+return 0;
+}
