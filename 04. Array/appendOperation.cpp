bc706cbf7202d5b815ac290be04880cde961d6cf
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-01-16 21:29:57 +0530

    Create appendOperation.cpp

---
+++ 04. Array/appendOperation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[10]={0,5,225,25,4};
+
+    // adding element in last index of an array
+
+    arr[5]=45;
+    cout<<arr[5];
+
+return 0;
+}
